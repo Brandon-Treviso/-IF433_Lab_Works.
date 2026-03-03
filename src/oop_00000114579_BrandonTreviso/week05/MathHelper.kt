07b8346c5b61fc063ca5a625ee9b4df660207cb1
Brandon-Treviso30 <brandon.treviso@student.umn.ac.id>
2026-03-03 13:06:40 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000114579_BrandonTreviso/week05/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_00000114579_BrandonTreviso.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(JariJari: Double): Double {
+        return 3.14 * JariJari * JariJari
+    }
+}
