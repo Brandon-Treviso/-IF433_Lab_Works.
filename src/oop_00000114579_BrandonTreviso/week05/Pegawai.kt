3538d1bf73412a647fb89c4ec3e40ab087b457df
Brandon-Treviso30 <brandon.treviso@student.umn.ac.id>
2026-03-03 12:47:25 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000114579_BrandonTreviso/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000114579_BrandonTreviso.week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
