6771156cbf72c576277d02f23b9fce12d3da88c4
Brandon-Treviso30 <brandon.treviso@student.umn.ac.id>
2026-03-03 13:24:30 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_00000114579_BrandonTreviso/week05/CreditCard.kt
@@ -0,0 +1,14 @@
+package oop_00000114579_BrandonTreviso.week05
+
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Pembayaran berhasil menggunakan Credit Card. Total terpakai: $usedAmount")
+        } else {
+            println("Transaksi ditolak. Melebihi limit kartu kredit.")
+        }
+    }
+}
