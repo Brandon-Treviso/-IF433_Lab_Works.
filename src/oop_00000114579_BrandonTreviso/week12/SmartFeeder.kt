1305b1443a59158c4c8ab72df980bee4971d548a
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-05-12 14:44:21 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_00000114579_BrandonTreviso/week12/SmartFeeder.kt
+++ src/oop_00000114579_BrandonTreviso/week12/SmartFeeder.kt
@@ -64,5 +64,8 @@ fun main() {
             isJammed = false
         )
 
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }
 }
