7f2bb5c14d103d1ccda053825eed47cac0057bc5
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-05-12 14:37:57 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000114579_BrandonTreviso/week12/SmartFeeder.kt
+++ src/oop_00000114579_BrandonTreviso/week12/SmartFeeder.kt
@@ -14,5 +14,12 @@ fun dispenseKibble(
         throw DispenserJamException()
     }
 
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+
+
     return availableGram - requestedGram
 }
