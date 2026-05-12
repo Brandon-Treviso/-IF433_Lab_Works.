304e2804a533f5934ec8babf2b90c69cee039a37
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-05-12 14:41:03 +0700

    week12: (task) implement finally block for feeder logging

--- src/oop_00000114579_BrandonTreviso/week12/SmartFeeder.kt
+++ src/oop_00000114579_BrandonTreviso/week12/SmartFeeder.kt
@@ -49,6 +49,8 @@ fun main() {
     } catch (e: Exception) {
 
         println("Terjadi kesalahan umum: ${e.message}")
+    } finally {
+        println("Siklus pengecekan dispenser pagi selesai.")
     }
 }
 
