704d9005b70bbef6816d0e20334668ea5ab66ce1
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-05-26 15:25:11 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000114579_BrandonTreviso/week14/EcommerceTask.kt
+++ src/oop_00000114579_BrandonTreviso/week14/EcommerceTask.kt
@@ -2,21 +2,74 @@ package oop_00000114579_BrandonTreviso.week14
 
 import java.io.File
 
-class BadOrderProcessor {
-    private val file = File("orders.csv")
+interface OrderRepository {
+    fun saveOrder(data: String)
+}
 
-    fun processOrder (itemName: String, basePrice: Double, customerType: String) {
+class CsvOrderRepository : OrderRepository {
+
+    override fun saveOrder(data: String) {
+
+        File("orders.csv")
+            .bufferedWriter()
+            .use { writer ->
+                writer.append(data)
+                writer.newLine()
+            }
+
+        println("Order saved to CSV")
+    }
+}
+
+interface NotificationService {
+    fun sendNotification(email: String)
+}
+
+class EmailNotifier : NotificationService {
+
+    override fun sendNotification(email: String) {
+        println("Sending email notification to $email")
+    }
+}
+
+class SafeOrderProcessor(
+    private val repository: OrderRepository,
+    private val notifier: NotificationService
+) {
+
+    fun processOrder(
+        customerType: String,
+        customerEmail: String,
+        price: Double
+    ) {
 
         val finalPrice = when (customerType) {
-            "REGULAR" -> basePrice
-            "VIP" -> basePrice * 0.90 // Diskon 10%
-            else -> basePrice
+            "VIP" -> price * 0.8
+            "REGULAR" -> price * 0.9
+            else -> price
         }
 
-        println("Memproses pesanan $itemName seharga $finalPrice")
+        val orderData =
+            "Email: $customerEmail, Total: $finalPrice"
 
-        file.appendText("$itemName, $finalPrice, $customerType\n")
+        repository.saveOrder(orderData)
 
-        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+        notifier.sendNotification(customerEmail)
+
+        println("Final Price: $finalPrice")
     }
+}
+
+fun main() {
+
+    val processor = SafeOrderProcessor(
+        CsvOrderRepository(),
+        EmailNotifier()
+    )
+
+    processor.processOrder(
+        "VIP",
+        "brandon@gmail.com",
+        100000.0
+    )
 }
